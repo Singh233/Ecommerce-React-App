29b3147955d7655bc3feb637d4d90409c85d3c1c
Sanambir Singh <sanambir123@gmail.com>
2023-03-02 15:28:12 +0530

    Design navbar component

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,8 +1,39 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
+
+import styles from '../styles/Navbar.module.scss'
+
+// fontawesome for icons
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faBagShopping } from '@fortawesome/free-solid-svg-icons'
+import { faCircleUser } from '@fortawesome/free-solid-svg-icons'
 
 export default function Navbar() {
 
     return (
-        <div>Navbar</div>
+        <div className={styles.navContainer}>
+
+            <div className={styles.branding}>
+                <div className={styles.square}></div>
+                <p> <span>S</span>hop<span>W</span>ave</p>
+            </div>
+
+            <div className={styles.navLinks}>
+                <Link to="/">Home</Link>
+                <Link to="/about">Products</Link>
+                <Link to="/contact">Add Product</Link>
+            </div>
+
+            <div className={styles.navIcons}>
+                <Link to="/cart">
+                    <FontAwesomeIcon icon={faBagShopping} />
+                </Link>
+                <Link to="/user">
+                    <FontAwesomeIcon icon={faCircleUser} />
+                </Link>
+            </div>
+
+            
+        </div>
     )
 }
