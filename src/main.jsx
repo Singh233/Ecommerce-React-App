53de6fc1f3501f44e4306d816fc48e33394faedd
Sanambir Singh <sanambir123@gmail.com>
2023-03-02 13:22:17 +0530

    Add React routes and Navbar/Home components

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,16 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './components/App'
 import './styles/index.css'
+// React Router
+import { BrowserRouter as Router } from 'react-router-dom';
+
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <Router>
+
+      <App />
+
+    </Router>
   </React.StrictMode>,
 )
