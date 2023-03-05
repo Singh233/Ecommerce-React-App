3b97fdb0b10788c1fae765cff8f1f5d5732d25f3
Sanambir Singh <sanambir123@gmail.com>
2023-03-05 22:46:04 +0530

    Add product using API

--- src/api/index.js
+++ src/api/index.js
@@ -1,6 +1,19 @@
 const url = 'https://my-json-server.typicode.com/singh233/JSON-Server'
 
 // fetch products from url
-export const getProducts = () => {
-    return fetch(`${url}/products`).then(res => res.json())
+export const getProducts = async () => {
+    const res = await fetch(`${url}/products`)
+    return await res.json()
+}
+
+// post product to url
+export const postProduct = async (product) => {
+    const res = await fetch(`${url}/products`, {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(product)
+    })
+    return await res.json()
 }
