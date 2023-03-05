04af0d8f7d9da1a45578468d3d35d8148872d9c8
Sanambir Singh <sanambir123@gmail.com>
2023-03-05 22:14:39 +0530

    Design add product page and form

---
+++ src/components/AddProduct/StepOne.jsx
@@ -0,0 +1,79 @@
+import React, { useEffect, useState } from 'react';
+
+import styles from '../../styles/AddProduct/StepOne.module.scss';
+
+// fontawesome for icons
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faPenToSquare } from '@fortawesome/free-solid-svg-icons';
+import { faTrash } from '@fortawesome/free-solid-svg-icons';
+import { faBagShopping } from '@fortawesome/free-solid-svg-icons';
+import { faChevronUp } from '@fortawesome/free-solid-svg-icons';
+import { faChevronDown } from '@fortawesome/free-solid-svg-icons';
+
+import 'animate.css';
+
+// icons
+import categoryIcon from '../../assets/icons/category.svg';
+import starIcon from '../../assets/icons/star.svg';
+import sortIcon from '../../assets/icons/sorting.svg';
+
+export default function StepOne(props) {
+    const { productDetails, setProductDetails } = props.stateAsProp;
+
+
+
+
+    const handleNameInputChange = (e) => {
+        setProductDetails({
+            ...productDetails,
+            name: e.target.value,
+        });
+        console.log(productDetails)
+    };
+
+    const handlePriceInputChange = (e) => {
+        setProductDetails({
+            ...productDetails,
+            price: e.target.value,
+        });
+        console.log(productDetails)
+
+    };
+
+    const handleValidation = (e) => {
+        if (e.target.value === '') {
+            e.target.className = styles.error;
+        } else {
+            e.target.className = styles.success;
+
+        }
+    };
+
+    return (
+        <div className={styles.container}>
+            <div className={styles.left}>
+                <p>Step One</p>
+            </div>
+
+            <div className={styles.right}>
+                <label htmlFor="product-name">Product name</label>
+                <input
+                    onBlur={handleValidation}
+                    onChange={handleNameInputChange}
+                    value={productDetails.name}
+                    type="text"
+                    placeholder="Iphone 14 pro max"
+                />
+
+                <label className={styles.label2} htmlFor="product-price">
+                    Product Price ($)
+                </label>
+                <input 
+                    onChange={handlePriceInputChange}
+                    onBlur={handleValidation}
+                    value={productDetails.price}
+                    type="number" placeholder="Enter price in dollars" />
+            </div>
+        </div>
+    );
+}
